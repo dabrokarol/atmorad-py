a6c42a7da4e04a2bd5ccb1ecbb5a3ab90b30d4e2
Karol Dabrowski <dabrowskikarol998@gmail.com>
2026-05-05 22:53:35 +0200

    refactor: clean up comments

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import tomllib
 
-
-### OBJECTS USED TO CREATE AND RUN THE SIMULATION:
 from src.simulation import MCRadiation
 from src.scene import Scene, Space
 from src.atmosphere import Atmosphere, AtmosphericLayer, AtmosphericMedium
@@ -18,55 +16,36 @@ def read_config(path = 'config.toml'):
     return data
 
 if __name__ == '__main__':
-    ## Parse `config.toml`
     config = read_config()
     
-    ## Create atmospheric mediums: AtmosphericMedium(optical_density_per_km, single_scattering_albedo, scattering_object)
-    ## Scattering objects are kept in AtmosphereScatterings and take in assymetry parameter g as an argument upon creation
+    # 1. ATMOSPHERE
+    # Format: AtmosphericMedium(optical_density_per_km, albedo, scattering_phase_function)
     air = AtmosphericMedium(0.001, 0.5, AtmosphereScatterings.HenyeyGreenstein(g=0.5))
     clouds = AtmosphericMedium(20, 0.999999, AtmosphereScatterings.HenyeyGreenstein(g=0.85))
 
-    ## Compose layers with above definec materials: 
-    ## AtmosphericLayer(height_km, [(medium1, percentage1), (medium2, percentage2), ...])
-    ## Each layer consists of list of tuples: (medium, percentage); percentages should sum to 1
-    ## Eg. layer = AtmosphericLayer(50, [(air, 0.3), (clouds, 0.7)]) 
+    # Format: AtmosphericLayer(height_km, [(medium0, fraction0), (medium1, fraction1), ...])
+    # fractions should sum up to 1.0
+    # Eg. layer = AtmosphericLayer(50, [(air, 0.3), (clouds, 0.7)]) 
     layer0 = AtmosphericLayer(50, [(air, 1)])
     layer1 = AtmosphericLayer(1, [(clouds, 1)])
     layer2 = AtmosphericLayer(50, [(air, 1)])
-
-    ## Add layers to Atmosphere class
-    ## Atmosphere([layer1, layer2, layer3])
     atm = Atmosphere([layer0, layer1, layer2])
 
-
-    ## Create a surface material: SurfaceMaterial(albedo, reflection_object)
-    ## Reflection objects are kept in SurfaceReflections class and should be called upon initialization eg:
+    # 2. SURFACE
+    # Format: SurfaceMaterial(albedo, reflection_object)
     material0 = SurfaceMaterial(1, SurfaceReflections.MirrorReflection())
     material1 = SurfaceMaterial(0, SurfaceReflections.LambertianReflection())
 
-    ## Create a map: 
-    ## Procedural map takes in a function that takes in a np.array of shape (2, N) or (3, N) and outputs array of shape (N) with numbers
-    ## You can test prefefined Procedural maps, eg. ProceduralMap(ProceduralMap.checkerboard)
-    map = ProceduralMap(ProceduralMap.split_half_x)
+    # Procedural map takes in a function that takes in a np.array of shape (2, N) or (3, N) and outputs array of shape (N) with integers
+    # The returned integer array represents material IDs.
+    ground_map = ProceduralMap(ProceduralMap.split_half_x)
+    surface = Surface(ground_map, [material0, material1])
 
-    ## Surface is a class that keeps a map and list of surface materials
-    surface = Surface(map, [material0, material1])
-
-    ## For now an empty object, does nothing
-    space = Space()
-
-    ## Scene keeps information about simulation environment, create as follows:
-    ## Scene(surface, atmosphere, space, config)
+    # 3. SCENE AND SIMULATION
+    space = Space() # For now an empty object, represents end of atmosphere
     scene = Scene(surface, atm, space, config)
-
-    ## Simulation is an object that takes in config and scene
     sim = MCRadiation(config, scene)
 
-    ## Run simulation
     sim.run()
-
-    ## USEFUL FUNCTIONS:
-    ## Print results to command line
     sim.print_results()
-    ## Plot paths of `n_track` photons
     sim.plot_paths()
