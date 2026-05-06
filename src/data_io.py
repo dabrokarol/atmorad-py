df7e972207ff7e725aeedfc0ae74ffa48fcc9506
Karol Dabrowski <dabrowskikarol998@gmail.com>
2026-05-06 19:14:22 +0200

    feat: new plots for surface and vertical flux

--- src/data_io.py
+++ src/data_io.py
@@ -1,48 +1,55 @@
 import logging
-import time
+import datetime
 import json
 import tomllib
 import numpy as np
 
 from src.results import Results
 from pathlib import Path
+from typing import Any
+from matplotlib.figure import Figure
 
 class OutputHandler:
-    def __init__(self, base_dir: str, overwrite = False):
+    def __init__(self, base_dir: str, overwrite = False) -> None:
         self.base_dir = Path.cwd() / base_dir
         if overwrite:
             self.base_dir.mkdir(exist_ok=True)
         else:
             try:
                 self.base_dir.mkdir()
-            except Exception:
+            except FileExistsError:
                 logging.info('directory exists, adding timestamp...')
-                self.base_dir = Path.cwd() / (base_dir + f"{time.ctime().replace(' ', '-')}")
+                timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+                self.base_dir = Path.cwd() / f"{base_dir}_{timestamp}"
                 self.base_dir.mkdir()
     
-    def save_metadata(self, config, execution_time_s: float):
+    def save_metadata(self, config: dict[str, Any], execution_time_s: float) -> None:
         config['execution_time_s'] = execution_time_s
         with open(self.base_dir / 'metadata.json', 'w') as f:
             json.dump(config, f)
 
-    def save_plot(self, fig, plot_name: str):
+    def save_plot(self, fig: Figure, plot_name: str) -> None:
         fig.savefig(self.base_dir / plot_name, dpi=300, bbox_inches='tight')
 
-    def save_results(self, results: Results):
+    def save_results(self, results: Results) -> None:
         np.savez_compressed(
             self.base_dir / 'data_compressed',
             final_positions=results.final_positions,
             space_mask=results.space_mask,
             surface_mask=results.surface_mask,
-            atmosphere_mask=results.atmosphere_mask,
             layer_idx=results.layer_idx,
-            sample_paths=results.sample_paths
+            sample_paths=np.array(results.sample_paths, dtype=object),
+            surface_hits=results.surface_hits,
+            scatter_counts=results.scatter_counts,
+            measure_z=results.measure_z,
+            flux_up=results.flux_up,
+            flux_down=results.flux_down
         )
 
-    def print_results(self, results):
-        print(results)
+    def print_results(self, results: Results) -> None:
+        print(results.summary())
     
-def read_config(path = 'config.toml'):
+def read_config(path: Path|str = 'config.toml') -> dict[str, Any]:
     try:
         with open(path, 'rb') as f:
             data = tomllib.load(f)
@@ -50,9 +57,24 @@ def read_config(path = 'config.toml'):
         raise FileNotFoundError(f"config file missing at {path}")
     return data
 
-def read_results(path):
+def read_results(path: Path|str) -> Results:
     try:
-        data = np.fromfile(path)
+        data = np.load(path, allow_pickle=True)
+        res = Results(
+            final_positions=data['final_positions'],
+            space_mask=data['space_mask'],
+            surface_mask=data['surface_mask'],
+            layer_idx=data['layer_idx'],
+            sample_paths=data['sample_paths'].item(), # .item() because it was a dict dumped to np.array
+            surface_hits=data['surface_hits'],
+            scatter_counts=data['scatter_counts'],
+            measure_z=data['measure_z'],
+            flux_up=data['flux_up'],
+            flux_down=data['flux_down']
+        )
+
     except FileNotFoundError:
         raise FileNotFoundError(f"data file missing at {path}")
-    return data
+    except KeyError as e:
+        raise KeyError(f"Outdated or broken data file. Missing key: {e}")
+    return res
--- src/results.py
+++ src/results.py
@@ -2,14 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datashader as ds
 import seaborn as sns
-import colorcet
+import colorcet as cc
+import cmocean as cmo
 import pandas as pd
 
 from matplotlib.legend_handler import HandlerTuple
 from matplotlib.colors import ListedColormap
 from dataclasses import dataclass
 
-sns.set_theme(style="ticks")
+sns.set_theme(style="ticks", rc={"font.family": "serif"})
 
 @dataclass
 class Results:
@@ -27,8 +28,9 @@ class Results:
     def __post_init__(self):
         self.atmosphere_mask = (~self.space_mask) & (~self.surface_mask)
 
-    def __str__(self):
-        return f"photons left atmosphere: {np.count_nonzero(self.space_mask)}\n\
+    def summary(self):
+        return f"---- Simulation results ----\n\
+photons left atmosphere: {np.count_nonzero(self.space_mask)}\n\
 photons absorbed by surface: {np.count_nonzero(self.surface_mask)}\n\
 photons absorbed by atmosphere {np.count_nonzero(self.atmosphere_mask)}\n"
 
@@ -67,10 +69,55 @@ photons absorbed by atmosphere {np.count_nonzero(self.atmosphere_mask)}\n"
         ax.legend()
         return fig
 
-    def surface_plot(self):
-        pos = self.final_positions[:, self.surface_mask]
-        pos = pos[:, (pos[0]>-30) & (pos[0]<30) & (pos[1] > -30) & (pos[1]<30)]
-        joint_plot = sns.jointplot(x=pos[0], y=pos[1], kind="hex", cmap='viridis')
-        return joint_plot.figure
+    def surface_absorption_plot(self, limit=40):
+        return self._2dhexplot(self.final_positions[:, self.surface_mask], limit, 'Photons absorbed by the ground')
+        
+    def surface_flux_plot(self, limit=40):
+        return self._2dhexplot(self.surface_hits, limit, 'Downward flux near the ground')
+    
+    def _2dhexplot(self, pos, limit, title):
 
+        plot_mask = (pos[0] > -limit) & (pos[0] < limit) & (pos[1] > -limit) & (pos[1] < limit)
+        pos = pos[:, plot_mask]
+        color = cmo.cm.solar(2) # type: ignore
+        joint_plot = sns.jointplot(x=pos[0], y=pos[1], kind="hex", cmap=cmo.cm.solar, color=color) # type: ignore
+
+        joint_plot.ax_joint.set_xbound(-limit, limit)
+        joint_plot.ax_joint.set_ybound(-limit, limit)
+        mappable = joint_plot.ax_joint.collections[0]
+        joint_plot.ax_joint.figure.colorbar(mappable, label='Photon count', ax=[joint_plot.ax_joint, joint_plot.ax_marg_x, joint_plot.ax_marg_y], orientation='horizontal')
+        joint_plot.ax_joint.set_xlabel('Position (X)')
+        joint_plot.ax_joint.set_ylabel('Position (Y)')
+        joint_plot.ax_joint.set_title(title, fontsize=14, pad=70)
+
+        return joint_plot.figure
+    
+    def plot_flux_profile(self):
+        fig, ax = plt.subplots(figsize=(8, 10))
+        net_flux = self.flux_down - self.flux_up
+
+        ax.plot(self.flux_down, self.measure_z, label=r'Downward flux ($F^\downarrow$)', color='tab:blue', linewidth=2)
+        ax.plot(self.flux_up, self.measure_z, label=r'Upward flux ($F^\uparrow$)', color='tab:orange', linewidth=2)
+        ax.plot(net_flux, self.measure_z, label=r'Net flux ($F_{net}$)', color='black', linestyle='--', linewidth=2.5)
+
+        ax.set_xlabel("Flux [photon count]", fontsize=12)
+        ax.set_ylabel("Height (Z)", fontsize=12)
+
+        ax.invert_yaxis()
+        ax.grid(True, linestyle=':', alpha=0.7)
+        ax.legend(fontsize=11)
+        
+        ax.fill_betweenx(self.measure_z, 0, net_flux, color='gray', alpha=0.1)
+
+        fig.tight_layout()
+        return fig
+
+    def plot_scattering_histogram(self):
+        fig, ax = plt.subplots(figsize=(10, 6))
+        color = cmo.cm.solar(2) # type: ignore
+        sns.histplot(self.scatter_counts, ax=ax, color=color, kde=True)
+        ax.set_title('Photon scatterings')
+        ax.set_xlabel('Number of scatterings')
+        ax.set_ylabel('Number of photons')
+        return fig    
     
