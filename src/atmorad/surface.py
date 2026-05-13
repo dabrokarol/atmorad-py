766df75c0f4fd8aa1409327df9fcd58bc12c62f2
Karol Dabrowski <dabrowskikarol998@gmail.com>
2026-05-13 02:01:44 +0200

    refactor: tidy-up surface.py and comment-out gridMap as it it not yet tested

--- src/atmorad/surface.py
+++ src/atmorad/surface.py
@@ -1,13 +1,25 @@
 import numpy as np
 
+from typing import Sequence
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
 from atmorad.physics.reflection import SurfaceReflection
 
+@dataclass
 class SurfaceMaterial:
-    def __init__(self, albedo: float, reflection: SurfaceReflection):
-        self.albedo = albedo
-        self.reflection = reflection
+    albedo: float
+    reflection: SurfaceReflection
+    def __post_init__(self):
+        if not 0.0 <= self.albedo <= 1.0:
+            raise ValueError(f'Albedo should be non-negative, got {self.albedo}.')
 
-class ProceduralMap:
+class SurfaceMap(ABC):
+    @abstractmethod
+    def get_material_ids(self, pos: np.ndarray) -> np.ndarray:
+        ...
+
+class ProceduralMap(SurfaceMap):
     def __init__(self, procedure):
         self.procedure = procedure
 
@@ -33,39 +45,38 @@ class ProceduralMap:
         y = np.mod(pos[1], 10)
         return np.where(((x<5) & (y<5)) | ((x >= 5) & (y >= 5)), 0, 1)
 
-class GridMap:
-    def __init__(self, ground_ids_matrix, periodic = True, grid_density=10):
-        self.matrix = ground_ids_matrix
-        self.periodic = periodic
-        self.density = grid_density
-        self.dims = ground_ids_matrix.shape / grid_density
+# class GridMap(SurfaceMap):
+#     def __init__(self, ground_ids_matrix, periodic = True, grid_density=10):
+#         self.matrix = ground_ids_matrix
+#         self.periodic = periodic
+#         self.density = grid_density
+#         self.dims = np.array(ground_ids_matrix.shape) / grid_density
         
-    def get_material_ids(self, pos):
-        if self.periodic:
-            cords = np.mod(np.floor(pos[:2] * self.density).astype(int), self.dims[:, np.newaxis])
-            return self.matrix[cords[0], cords[1]]
-        else:
-            raise FutureWarning('Non-periodic ground map is not yet implemented')
+#     def get_material_ids(self, pos):
+#         if self.periodic:
+#             cords = np.mod(np.floor(pos[:2] * self.density).astype(int), self.dims[:, np.newaxis])
+#             return self.matrix[cords[0], cords[1]]
+#         else:
+#             raise NotImplementedError('Non-periodic ground map is not yet implemented')
 
 class Surface:
-    def __init__(self, ground_map: GridMap | ProceduralMap, ground_types: list[SurfaceMaterial]):
+    def __init__(self, ground_map: SurfaceMap, ground_types: Sequence[SurfaceMaterial]):
         self.ground_map = ground_map
         self.ground_types = np.array(ground_types)
         self.albedos = np.array([material.albedo for material in self.ground_types])
         self.reflections = [material.reflection for material in self.ground_types]
-
-    def check_reflection(self, pos, rand):
-        ground_ids = self.ground_map.get_material_ids(pos)
-        albedos = self.albedos[ground_ids]
-
-        return albedos > rand
     
-    def reflect(self, pos, direction, rand_1, rand_2):
+    def check_and_reflect(self, pos, direction, rand_albedo, rand_theta, rand_phi):
         ground_ids = self.ground_map.get_material_ids(pos)
+        
+        albedos = self.albedos[ground_ids]
+        reflect_mask = albedos > rand_albedo
+
         result_direction = np.zeros_like(direction)
 
         for i, refl in enumerate(self.reflections):
-            mask_i = ground_ids == i
-            result_direction[:, mask_i] = refl.reflect(direction[:, mask_i], rand_1[mask_i], rand_2[mask_i])
+            mask_i = (ground_ids == i) & reflect_mask
 
-        return result_direction
+            result_direction[:, mask_i] = refl.reflect(direction[:, mask_i], rand_theta[mask_i], rand_phi[mask_i])
+
+        return reflect_mask, result_direction
