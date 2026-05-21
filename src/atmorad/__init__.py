f13c9dd293fa48b41cdd3425e2a1eab29e738d8f
Karol Dabrowski <dabrowskikarol998@gmail.com>
2026-05-21 02:01:47 +0200

    refactor: add rotate and orientation to public api

--- src/atmorad/__init__.py
+++ src/atmorad/__init__.py
@@ -1,7 +1,7 @@
 from .builder import build_context
 from .engine.runner import MCRadiationRunner
 from .output import DataIO, ResultAnalyzer
-from .physics import Scattering, SurfaceReflection
+from .physics import Scattering, SurfaceReflection, orientation, rotate
 from .physics.registry import register_reflection, register_scattering
 
 __all__ = [
@@ -13,4 +13,6 @@ __all__ = [
     "ResultAnalyzer",
     "Scattering",
     "SurfaceReflection",
+    "orientation",
+    "rotate",
 ]
