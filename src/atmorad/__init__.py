90bff81ba1e84815c37629eaef0a5982fabd2029
Karol Dabrowski <dabrowskikarol998@gmail.com>
2026-05-23 15:01:03 +0200

    style: format file

--- src/atmorad/__init__.py
+++ src/atmorad/__init__.py
@@ -1,35 +1,31 @@
-from .api import run, load
-
+from .api import load, run
+from .constants import X, Y, Z
+from .detectors import BaseDetector
+from .environment import BaseSurfaceMap
+from .physics import Scattering, SurfaceReflection
 from .registry import (
     register_detector,
     register_reflection,
     register_scattering,
     register_surface_map,
 )
-from .detectors import BaseDetector
-from .physics import Scattering
-from .physics import SurfaceReflection
-from .environment import BaseSurfaceMap
-
-from .constants import X, Y, Z
 
 __all__ = [
     # Main API
     "run",
     "load",
-    
     # Registry decorators
     "register_detector",
     "register_reflection",
     "register_scattering",
     "register_surface_map",
-    
     # Base Classes
     "BaseDetector",
     "Scattering",
     "SurfaceReflection",
     "BaseSurfaceMap",
-    
     # Constants
-    "X", "Y", "Z",
+    "X",
+    "Y",
+    "Z",
 ]
