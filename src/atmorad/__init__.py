fc0c30a7381d6143f7c536764247ccaa768eeab4
Karol Dabrowski <dabrowskikarol998@gmail.com>
2026-05-23 15:00:32 +0200

    chore: update public API

--- src/atmorad/__init__.py
+++ src/atmorad/__init__.py
@@ -1,22 +1,35 @@
-from .api import load, run
-from .environment import BaseSurfaceMap
-from .physics import (
-    Scattering,
-    SurfaceReflection,
-    orientation,
-    rotate,
+from .api import run, load
+
+from .registry import (
+    register_detector,
+    register_reflection,
+    register_scattering,
+    register_surface_map,
 )
-from .registry import register_reflection, register_scattering, register_surface_map
+from .detectors import BaseDetector
+from .physics import Scattering
+from .physics import SurfaceReflection
+from .environment import BaseSurfaceMap
+
+from .constants import X, Y, Z
 
 __all__ = [
+    # Main API
+    "run",
+    "load",
+    
+    # Registry decorators
+    "register_detector",
     "register_reflection",
     "register_scattering",
     "register_surface_map",
+    
+    # Base Classes
+    "BaseDetector",
     "Scattering",
     "SurfaceReflection",
     "BaseSurfaceMap",
-    "orientation",
-    "rotate",
-    "run",
-    "load",
+    
+    # Constants
+    "X", "Y", "Z",
 ]
